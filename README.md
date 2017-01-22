9f074b7fdee0c8cc3719e9d1e98f01b3d06d3668
innossh <innossh@users.noreply.github.com>
2017-01-22 16:22:16 +0900

    Monitor dummy response time and dummy hook delivery time

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # serverless-apm-example
+
+This is an example of the application performance management by AWS Lambda.
+
+## Deployment
+
+```
+$ serverless deploy
+```
+
+That's it :)
--- handler.py
+++ handler.py
@@ -1,22 +1,57 @@
-import json
+from __future__ import print_function
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+import boto3
+import random
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+print('Loading function')
+cw = boto3.client('cloudwatch')
 
-    return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+def monitor_response_time(event, context):
+    elapsed_time = -1
+    try:
+        elapsed_time = test_response()
+        put_cloudwatch_metric('response_time', elapsed_time, get_current_region(context))
+    except Exception as e:
+        print("Exception: " + str(e))
+    return elapsed_time
+
+
+def monitor_hook_delivery_time(event, context):
+    elapsed_time = -1
+    try:
+        elapsed_time = test_hook()
+        put_cloudwatch_metric('hook_delivery_time', elapsed_time, get_current_region(context))
+    except Exception as e:
+        print("Exception: " + str(e))
+    return elapsed_time
+
+
+def test_response():
+    return random.randint(100, 500)
+
+
+def test_hook():
+    return random.randint(500, 2000)
+
+
+def get_current_region(context):
+    return context.invoked_function_arn.split(":")[3]
+
+
+def put_cloudwatch_metric(metric_name, value, region):
+    print("Sending custom metrics {0}:{1}ms".format(metric_name, value))
+    cw.put_metric_data(
+        Namespace='Custom/APM',
+        MetricData=[{
+            'MetricName': metric_name,
+            'Value': value,
+            'Unit': 'Milliseconds',
+            'Dimensions': [
+                {
+                    'Name': 'Region',
+                    'Value': region
+                }
+            ]
+        }]
+    )
